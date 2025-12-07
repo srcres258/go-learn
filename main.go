5889e584af2e5f01920c5fda8c98e3771ca4ee32
src_resources <src.res.211@gmail.com>
2025-12-07 10:43:56 +0800

    Update

--- main.go
+++ main.go
@@ -2,41 +2,24 @@ package main
 
 import "fmt"
 
-type Class struct {
+type Animal struct {
 	Name string
 }
 
-type Student struct {
-	Class
-	Name string
+type Animal1 struct {
+	Name1 string
 }
 
-func (s *Student)Study() {
-	fmt.Printf("%s is learning...\n", s.Name)
-}
-func (s *Student)Info() {
-	fmt.Printf("Name: %s, Class: %s\n", s.Name, s.Class.Name);
-}
-
-func (s *Student) SetName(name string) {
-	s.Name = name
+type Cat struct {
+	Animal
+	Animal1
 }
 
 func main() {
-	c1 := Class {
-		Name: "Class 1",
+	var animal = Animal { Name: "Cat" }
+	cat := Cat {
+		Animal: animal,
 	}
-
-	s1 := Student { Class: c1, Name: "Alice" }
-	s1.Study()
-	s1.Info()
-
-	s2 := Student { Class: c1, Name: "Bob" }
-	s2.Study()
-	s2.Info()
-
-	fmt.Printf("%p\n", &s2)
-	s2.SetName("Charlie")
-	s2.Info()
+	fmt.Println(cat.Name, cat.Name1)
 }
 

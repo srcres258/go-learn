93013ef218a508326ddd4d25b5e6c93dce36324c
src_resources <src.res.211@gmail.com>
2025-12-07 21:14:55 +0800

    Update

--- main.go
+++ main.go
@@ -2,14 +2,60 @@ package main
 
 import "fmt"
 
-type MyCode int
-type MyAliasCode = int
+type SingInterface interface {
+	Sing()
+	GetName() string
+}
 
-const myCode MyCode = 1
-const myAliasCode MyAliasCode = 1
+type Chicken struct {
+	Name string
+}
+
+func (c Chicken) Sing() {
+	fmt.Println(c.Name, "is singing")
+}
+func (c Chicken) GetName() string {
+	return c.Name
+}
+
+type Cat struct {
+	Name string
+}
+
+func (c Cat) Sing() {
+	fmt.Println(c.Name, "is singing")
+}
+func (c Cat) GetName() string {
+	return c.Name
+}
+
+func sing(c SingInterface) {
+	c.Sing()
+	fmt.Println(c.GetName())
+
+	ch, ok := c.(Chicken)
+	fmt.Println(ch, ok)
+
+	switch server := c.(type) {
+	case Chicken:
+		fmt.Println("It's a chicken:", server.Name)
+	case Cat:
+		fmt.Println("It's a cat:", server.Name)
+	default:
+		fmt.Println("Unknown type")
+	}
+}
+
+func Print(v any) {
+	fmt.Println(v)
+}
 
 func main() {
-	fmt.Printf("%v,%T\n", myCode, myCode)
-	fmt.Printf("%v,%T\n", myAliasCode, myAliasCode)
+	ch := Chicken{Name: "Clucky"}
+	ca := Cat{Name: "Lihuamao"}
+	sing(ch)
+	sing(ca)
+	Print(ch)
+	Print(ca)
 }
 

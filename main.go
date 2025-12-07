afc255b128bbfc8c566edaf1497c35c23ca7fc5c
src_resources <src.res.211@gmail.com>
2025-12-07 22:12:58 +0800

    Update

--- main.go
+++ main.go
@@ -4,22 +4,48 @@ import "fmt"
 import "time"
 import "sync"
 
-func shopping(name string, wait *sync.WaitGroup) {
-  fmt.Println(name, "started shopping")
-  time.Sleep(1 * time.Second)
-  fmt.Println(name, "finished shopping")
-  wait.Done()
+var moneyChan = make(chan int)
+
+func pay(name string, money int, wait *sync.WaitGroup) {
+	fmt.Println(name, "started shopping")
+	time.Sleep(1 * time.Second)
+	fmt.Println(name, "finished shopping")
+
+	moneyChan <- money
+
+	wait.Done()
 }
 
 func main() {
-  start := time.Now()
+	start := time.Now()
+
 	var wait sync.WaitGroup
-  wait.Add(4)
-  go shopping("Alpha", &wait)
-  go shopping("Bravo", &wait)
-  go shopping("Charlie", &wait)
-  go shopping("Delta", &wait)
-  wait.Wait()
-  fmt.Println("All shopping done, time:", time.Since(start))
+
+	wait.Add(3)
+
+	go pay("Alpha", 2, &wait)
+	go pay("Bravo", 3, &wait)
+	go pay("Charlie", 5, &wait)
+
+	go func() {
+		wait.Wait()
+		close(moneyChan)
+	}()
+
+	// for {
+	// 	money, ok := <- moneyChan
+	// 	fmt.Println(money, ok)
+	// 	if !ok {
+	// 		break
+	// 	}
+	// }
+	var moneyList []int
+	for money := range moneyChan {
+		fmt.Println("Received money:", money)
+		moneyList = append(moneyList, money)
+	}
+
+	fmt.Println("All shopping done, time:", time.Since(start))
+	fmt.Println("Money received:", moneyList)
 }
 

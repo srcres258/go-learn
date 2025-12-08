966e8acdff0901baf5e754cdb8b5b4009b5960de
src_resources <src.res.211@gmail.com>
2025-12-08 21:27:28 +0800

    Update

--- main.go
+++ main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"errors"
 )
 
-func main() {
-	var maps = sync.Map{}
-	go func() {
-		for {
-			maps.Store(1, "hello")
-		}
-	}()
-	go func() {
-		for {
-			v, ok := maps.Load(1)
-			fmt.Println(v, ok)
-		}
-	}()
+func div(a, b int) (res int, err error) {
+	if b == 0 {
+		err = errors.New("除数不能为0")
+		return
+	}
+	res = a / b
+	return
+}
 
-	select{}
+func server() (res int, err error) {
+	res, err = div(2, 0)
+	if err != nil {
+		// 把错误向上传递
+		return
+	}
+
+	// 执行其他的逻辑
+	res++
+	res += 2
+
+	return
+}
+
+func main() {
+	res, err := server()
+	if err != nil {
+		fmt.Println("错误:", err)
+		return
+	}
+	fmt.Println("结果:", res)
 }
 

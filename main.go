1338c401653139450f1dc9164141d58b0625b00d
src_resources <src.res.211@gmail.com>
2025-12-08 21:32:47 +0800

    Update

--- main.go
+++ main.go
@@ -2,38 +2,17 @@ package main
 
 import (
 	"fmt"
-	"errors"
+	"os"
 )
 
-func div(a, b int) (res int, err error) {
-	if b == 0 {
-		err = errors.New("除数不能为0")
-		return
-	}
-	res = a / b
-	return
-}
-
-func server() (res int, err error) {
-	res, err = div(2, 0)
+func init() {
+	_, err := os.ReadFile("111")
 	if err != nil {
-		// 把错误向上传递
-		return
+		panic("错误了")
 	}
-
-	// 执行其他的逻辑
-	res++
-	res += 2
-
-	return
 }
 
 func main() {
-	res, err := server()
-	if err != nil {
-		fmt.Println("错误:", err)
-		return
-	}
-	fmt.Println("结果:", res)
+	fmt.Println("Hello, World!")
 }
 

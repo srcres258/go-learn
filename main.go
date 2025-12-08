1afe82ed6f288531f3e78891784497dd19715686
src_resources <src.res.211@gmail.com>
2025-12-08 22:19:29 +0800

    Update

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"runtime/debug"
 )
 
-func init() {
-	_, err := os.ReadFile("111")
-	if err != nil {
-		panic("错误了")
-	}
+func read() {
+	var list []int = []int {1, 2}
+	fmt.Println(list[2])
+}
+
+func main1() {
+	defer func() {
+		fmt.Println("Recovering from panic...")
+		err := recover()
+		if err != nil {
+			fmt.Println("Recovered error:", err)
+			fmt.Println("Stack trace:", string(debug.Stack()))
+		}
+	}()
+	read()
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	main1()
+
+	fmt.Println("End of main")
 }
 

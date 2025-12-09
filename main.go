7830a34ca274528c43691cd370e1fd560c2b1a8f
src_resources <src.res.211@gmail.com>
2025-12-09 22:31:51 +0800

    Update

--- main.go
+++ main.go
@@ -5,23 +5,25 @@ import (
 	"fmt"
 )
 
-func getValue(obj any) {
-	v := reflect.ValueOf(obj)
-	switch v.Kind() {
+func setValue(obj any, value any) {
+	v1 := reflect.ValueOf(obj)
+	v2 := reflect.ValueOf(value)
+	if v1.Elem().Kind() != v2.Kind() {
+		return
+	}
+	switch v1.Elem().Kind() {
 	case reflect.String:
-		fmt.Println("String", v.String())
+		v1.Elem().SetString(value.(string))
 	case reflect.Int:
-		fmt.Println("Int", v.Int())
-	case reflect.Struct:
-		fmt.Println("Struct")
+		v1.Elem().SetInt(v2.Int())
 	}
 }
 
 func main() {
-	getValue(114514)
-	getValue("114514")
-	getValue(struct {
-		Name string
-	}{})
+	var name = "田所"
+	var age = 24
+	setValue(&name, "德川")
+	setValue(&age, 25)
+	fmt.Println(name, age)
 }
 

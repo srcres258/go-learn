94e0e3dd05b541926ab3b2a8b5af690e0959d719
src_resources <src.res.211@gmail.com>
2025-12-09 21:57:52 +0800

    Update

--- main.go
+++ main.go
@@ -2,60 +2,50 @@ package main
 
 import (
 	"os"
-	"bufio"
+	// "bufio"
 	"fmt"
 	// "io"
 )
 
 func main() {
-	// byteData, err := os.ReadFile("111")
-	// byteData, err := os.ReadFile("/home/srcres/Coding/Learn/go-learn/test.txt")
-	// byteData, err := os.ReadFile("test.txt")
+	// file, err := os.OpenFile("w.txt", os.O_CREATE | os.O_WRONLY, 0777)
+	// file, err := os.OpenFile("w.txt", os.O_CREATE | os.O_RDWR, 0777)
 	// if err != nil {
-	// 	fmt.Println("Error reading file:", err)
+	// 	panic(err)
+	// }
+	// defer file.Close()
+	// // file.Write([]byte("你好"))
+	// byteData, err := io.ReadAll(file)
+	// if err != nil {
+	// 	fmt.Println(err)
 	// 	return
 	// }
-	// fmt.Println("File content:", string(byteData))
+	// fmt.Println(string(byteData))
 
-	// file, err := os.Open("test.txt")
+	// err := os.WriteFile("w1.txt", []byte("hello world"), 0666)
+	// fmt.Println(err)
+
+	// rFile, err := os.Open("/home/srcres/Pictures/0303f50b6eabbbce23_n.jpg")
 	// if err != nil {
-	// 	panic(err)
+	// 	fmt.Println(err)
+	// 	return
 	// }
-	// defer file.Close()
-	// for {
-	// 	var byteData = make([]byte, 15)
-	// 	n, err := file.Read(byteData)
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	fmt.Println(string(byteData[:n]))
-	// }
-
-	// file, err := os.Open("test.txt")
+	// defer rFile.Close()
+	// wFile, err := os.OpenFile("pic.jpg", os.O_CREATE | os.O_WRONLY, 0777)
 	// if err != nil {
-	// 	panic(err)
+	// 	fmt.Println(err)
+	// 	return
 	// }
-	// defer file.Close()
-	// buf := bufio.NewReader(file)
-	// for {
-	// 	line, _, err := buf.ReadLine()
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	fmt.Println(string(line))
-	// }
+	// io.Copy(wFile, rFile)
 
-	file, err := os.Open("test.txt")
+	dir, err := os.ReadDir("/home/srcres/Coding/Learn/go-learn")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	defer file.Close()
-	buf := bufio.NewScanner(file)
-	buf.Split(bufio.ScanWords)
-	var index int
-	for buf.Scan() {
-		index++
-		fmt.Println(index, buf.Text())
+	for _, entry := range dir {
+		info, _ := entry.Info()
+		fmt.Println(entry.IsDir(), entry.Name(), info.Name(), info.Size(), info.Mode())
 	}
 }
 
